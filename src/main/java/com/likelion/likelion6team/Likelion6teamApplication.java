5ca396940e7218bd26e17db3d684f8c8958fe4de
SunSeoYeon <ssy01157@gmail.com>
2025-07-07 14:31:47 +0900

    Initial commit

---
+++ src/main/java/com/likelion/likelion6team/Likelion6teamApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion6team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion6teamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Likelion6teamApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/likelion/likelion6team/Likelion6teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion6team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion6teamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
